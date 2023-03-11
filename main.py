f1c24d032b39ebb2591cd4b6431d940114e6fbf6
drotsen <marko.thoren@gmail.com>
2023-03-11 13:18:28 +0000

    Docker 3

--- main.py
+++ main.py
@@ -12,12 +12,14 @@ app = FastAPI()
 async def root():
     return {"message": "Wikipedia API.  Call /search or /wiki"}
 
+
 @app.get("/search/{value}")
 async def search(value: str):
     """Page to search in wikipedia"""
 
     result = search_wiki(value)
     return {"result": result}
+
 
 @app.get("/wiki/{name}")
 async def wiki(name: str):
@@ -37,5 +39,3 @@ async def phrase(name: str):
 
 if __name__ == "__main__":
     uvicorn.run(app, port=8080, host="0.0.0.0")
-
-
